ed35e362e78d7f7961b15c43b811f67363af16d1
sailecodes <eliasiv.codes@gmail.com>
2024-07-24 13:22:34 -0700

    build

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: ["https://saile-test-client.netlify.app/", "http://localhost:5173"],
+    origin: ["https://saile-test-client.netlify.app", "http://localhost:5173"],
     credentials: true,
   })
 );
